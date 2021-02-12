6aebb1667a1e7fd3e3c65ae2bd44d406513102d6
Tyson Trautmann <tysont@gmail.com>
2021-02-12 09:30:44 -0800

    moving exec fql from script into app, with error for now

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/pingcap/parser/test_driver"
 	"github.com/spf13/cobra"
 	"os"
+	"os/exec"
 )
 
 const art = `
@@ -20,23 +21,39 @@ const art = `
 var rootCommand = &cobra.Command{
 	Use:	"sql2fql",
 	Short:	art,
-	Run: transpileSqlToFql,
+	Run: run,
 }
+
+const shellCommand = "fauna"
 
 var sql string
 var optimize bool
+var key string
 
 func main() {
-	rootCommand.Flags().StringVarP(&sql, "sql", "s", "", "the SQL command")
+	rootCommand.Flags().StringVarP(&sql, "sql", "s", "", "the SQL shellCommand")
 	rootCommand.MarkFlagRequired("sql")
 	rootCommand.Flags().BoolVarP(&optimize, "optimize", "o", false, "whether to use indexes")
+	rootCommand.Flags().StringVarP(&key, "key", "k", "", "the key to use to run the query")
 	if err := rootCommand.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func transpileSqlToFql (cmd *cobra.Command, _ []string) {
+func run (cmd *cobra.Command, _ []string) {
+	fql := transpileSqlToFql(sql)
+	fmt.Println(fql)
+	if key != "" {
+		if !shellInstalled() {
+			panic(fmt.Sprintf("fauna shell isn't installed or configured correctly"))
+		}
+		out := executeFql(fql, key)
+		fmt.Println(out)
+	}
+}
+
+func transpileSqlToFql (sql string) string {
 	node, err := parseSql(sql)
 	if err != nil {
 		panic(fmt.Sprintf("error parsing sql: %s", err.Error()))
@@ -47,7 +64,7 @@ func transpileSqlToFql (cmd *cobra.Command, _ []string) {
 	} else {
 		ir = constructIR(node)
 	}
-	fmt.Println(ir.FQLRepr())
+	return ir.FQLRepr()
 }
 
 func parseSql(sql string) (*ast.StmtNode, error) {
@@ -57,4 +74,24 @@ func parseSql(sql string) (*ast.StmtNode, error) {
 		return nil, err
 	}
 	return &nodes[0], nil
+}
+
+func shellInstalled() bool {
+	cmd := exec.Command("command", "-v", shellCommand)
+	err := cmd.Run()
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func executeFql(fql string, key string) string {
+	cmd := exec.Command(shellCommand, "eval", fmt.Sprintf("--secret=\"%s\"", key), "--format=shell", fmt.Sprintf("\"%s\"", fql))
+	fmt.Println(cmd.String())
+	err := cmd.Run()
+	if err != nil {
+		panic(fmt.Sprintf("error executing fql: %s", err))
+	}
+	out, _ := cmd.Output()
+	return string(out)
 }
